a6e5ee291037b5a163ee5cac1e4558d3dd8a7806
AdamHalassyGfa <124493271+AdamHalassyGfa@users.noreply.github.com>
2023-10-24 19:51:55 +0200

    Create UserApiController.java

---
+++ src/com.noty.web/src/main/java/com/noty/web/controllers/api/UserApiController.java
@@ -0,0 +1,17 @@
+package com.noty.web.controllers.api;
+
+import com.noty.web.entities.User;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/user")
+public class UserApiController {
+
+    private final
+
+    @PostMapping("/")
+    public void createUser() {
+
+    }
+
+}
