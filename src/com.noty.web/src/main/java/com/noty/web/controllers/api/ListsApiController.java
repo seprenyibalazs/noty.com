59d2e62e3fdeaa0797d4b6486d13d6477d852661
Adam Halassy <adam.halassy@gmail.com>
2023-11-02 19:47:04 +0100

    Create list now uses form instead JSON body

--- src/com.noty.web/src/main/java/com/noty/web/controllers/api/ListsApiController.java
+++ src/com.noty.web/src/main/java/com/noty/web/controllers/api/ListsApiController.java
@@ -1,7 +1,6 @@
 package com.noty.web.controllers.api;
 
 import com.noty.web.NotyException;
-import com.noty.web.controllers.api.model.requests.NotyListCreateModel;
 import com.noty.web.controllers.api.model.response.NotyListResponse;
 import com.noty.web.entities.NotyList;
 import com.noty.web.services.ListProvider;
@@ -9,7 +8,6 @@ import com.noty.web.services.security.NotyImpersonation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,9 +21,9 @@ public class ListsApiController {
     @PostMapping("/")
     public NotyListResponse createList(
             @AuthenticationPrincipal NotyImpersonation impersonation,
-            @RequestBody NotyListCreateModel model
+            String title
     ) throws NotyException {
-        NotyList list = listProvider.createList(impersonation, model.getTitle());
+        NotyList list = listProvider.createList(impersonation, title);
         return NotyListResponse.fromList(list);
     }
 
